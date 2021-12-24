4758bff47af0b431ffbec590b00423dcf9c75033
Clairs-zx <hdu_zhangxin@163.com>
2021-12-24 22:45:15 +0800

    modified:   public/index.html 	modified:   src/App.vue 	new file:   src/components/AddTask.vue 	new file:   src/components/Button.vue 	new file:   src/components/Header.vue 	deleted:    src/components/HelloWorld.vue 	new file:   src/components/Task.vue 	new file:   src/components/Tasks.vue

---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+    <button @click="$emit('toggle-add-task')" :style="{background:color}" class="btn"> {{text}}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: String,
+        color: String,
+    },
+    emits: ['toggle-add-task'],
+
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button @toggle-add-task="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add Task'" :color="showAddTask ? 'pink' : 'skyblue'"/>
+        <!-- <Button text="Update Task" color="green"/>
+        <Button text="Delete Task" color="red"/> -->
+
+    </header>
+</template>
+
+<script>
+import Button from "./Button"
+export default {
+    name: 'Header',
+    components:{
+        Button
+    },
+    props: {
+        title: {
+            type: String,
+            default: 'Hello World'
+        },
+        showAddTask: Boolean,
+    },
+    emits:['toggle-add-task'],
+}
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
